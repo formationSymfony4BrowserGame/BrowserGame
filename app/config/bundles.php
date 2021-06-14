6be90aadb746c01b0bb25f1662b6d1d471283ce2
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2021-06-14 17:07:06 +0200

    correction problèmes du à webserver-bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
