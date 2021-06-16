4535a54f9047b7433067f0f1e42bc693650a77de
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2021-06-16 09:04:40 +0200

    gameController + templates associés

---
+++ app/src/Controller/GameController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    /**
+     * @Route("/game/play", name="play")
+     */
+    public function play(): Response
+    {
+        return $this->render('game/play.html.twig');
+    }
+
+    /**
+     * @Route("/game/new", name="new_game")
+     */
+    public function newGame(): Response
+    {
+        return $this->render('game/new.html.twig');
+    }
+
+    /**
+     * @Route("/game/load", name="load_game")
+     */
+    public function loadGame(): Response
+    {
+        return $this->render('game/load.html.twig');
+    }
+
+    /**
+     * @Route("/game/gamepage", name="game")
+     */
+    public function game(Request $request): Response
+    {
+        $get = $request->query->all();
+        dump($get['new']);
+        return $this->render('game/game.html.twig', [
+            ''
+        ]);
+    }
+}
