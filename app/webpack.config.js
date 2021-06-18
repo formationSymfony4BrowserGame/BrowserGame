4bcf8fa90fa9c1439054d0ed8c54796714c1009d
NarjesJOMAA <68105682+NarjesJOMAA@users.noreply.github.com>
2021-06-18 17:03:15 +0200

    Page d'erreur 404 personalisée

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -23,6 +23,7 @@ Encore
   .addEntry('app', './assets/app.js')
   .addStyleEntry('Bulma', './assets/styles/bulma.scss')
   .addStyleEntry('Styles', './assets/styles/styles.scss')
+  .addEntry('404', './assets/img/404.png')
 
 // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
   .enableStimulusBridge('./assets/controllers.json')
