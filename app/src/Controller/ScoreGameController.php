36a9b42a7788af3e679e5569a95e79cc1b512403
unknown <thranva@gmail.com>
2021-07-06 16:57:43 +0200

    update

--- app/src/Controller/ScoreGameController.php
+++ app/src/Controller/ScoreGameController.php
@@ -12,25 +12,79 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ScoreGameController extends AbstractController
 {
     /**
-     * @Route("/game/score", name="score_game")
+     * @Route("/score/{id}", name="score_game")
      */
-    public function scoreGame(): Response
+    public function scoreGame($id): Response
     {
-        return $this->render('game/scoreGame.html.twig');                
+        $game = $this->getDoctrine()
+            ->getRepository(Game::class)
+            ->find($id);
+
+        if (!$game) {
+            throw $this->createNotFoundException(
+                'Partie non trouvé '.$id
+            );
+        }
+        $players = $game->getPlayers();
+        $sortedPlayers = [];
+        foreach($players as $player){
+            $pickominos = $player->getPickominos();
+            $score = 0;
+            foreach($pickominos as $pickomino){
+                $score += floor(($pickomino - 21) / 4) + 1; 
+            }
+            $sortedPlayers[] = [
+                'pseudo' => $player->getPseudo(),
+                'score' => $score
+            ];
+        }
+        usort($sortedPlayers,function($a, $b){
+            if ($a["score"]>$b["score"]){
+                return -1;
+            }
+            else if ($a["score"]==$b["score"]){
+                return 0;
+            }
+            else if ($a["score"]<$b["score"]){
+                return 1;
+            }
+        });
+
+        return $this->render('score/scoreGame.html.twig',[
+            'game'=>$game,
+            'players'=>$sortedPlayers
+        ]);
+
     }
+    
 
     /**
-     * @Route("/game/score/{id}", name="delete_game", methods={"POST"})
+     * @Route("/score/delete/{id}", name="delete_game", methods={"POST"})
      */
-    public function deleteGame(Game $games, Request $request): Response
+    public function deleteGame($id , Request $request): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$games->getId(), $request->get('_token'))){
+        $game = $this->getDoctrine()
+            ->getRepository(Game::class)
+            ->find($id);
+
+        $user = $this->getuser();
+
+        //check if user is connected and if he's the game proprietary
+        if (empty($user)) {
+            return $this->render('accueil/index.html.twig', [
+                'message' => "Connectez-Vous pour pouvoir accéder à cette page",
+            ]);                
+        } else if ($user !== $game->getUser()) {
+            return $this->render('accueil/index.html.twig', [
+                'message' => "Cette partie ne vous appartient pas",
+            ]);    
+        }
+
+        if ($this->isCsrfTokenValid('delete'.$game->getId(), $request->get('_token'))){
             $em = $this->getDoctrine()->getManager();
-            $em->remove($games);
+            $em->remove($game);
             $em->flush();
         }
-        return $this->redirectToRoute('play', [
-            'games' => $games,
-        ]);
+        return $this->redirectToRoute('accueil');
     }
 }
